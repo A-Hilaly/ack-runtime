208fdf2c228c650ab5de6cdb1fec76cceec2270e
Amine <hilalyamine@gmail.com>
2022-10-25 17:16:07 -0400

    Update aws_resource_manager.go

--- pkg/types/aws_resource_manager.go
+++ pkg/types/aws_resource_manager.go
@@ -33,65 +33,14 @@ import (
 // Use an AWSResourceManagerFactory to create an AWSResourceManager for a
 // particular APIResource and AWS account.
 type AWSResourceManager interface {
-	// ReadOne returns the currently-observed state of the supplied AWSResource
-	// in the backend AWS service API.
-	//
-	// Implementers should return (nil, ackerrors.NotFound) when the backend
-	// AWS service API cannot find the resource identified by the supplied
-	// AWSResource's AWS identifier information.
 	ReadOne(context.Context, AWSResource) (AWSResource, error)
-	// Create attempts to create the supplied AWSResource in the backend AWS
-	// service API, returning an AWSResource representing the newly-created
-	// resource
 	Create(context.Context, AWSResource) (AWSResource, error)
-	// Update attempts to mutate the supplied desired AWSResource in the
-	// backend AWS service API, returning an AWSResource representing the
-	// newly-mutated resource.
-	// Note for specialized logic implementers can check to see how the latest
-	// observed resource differs from the supplied desired state. The
-	// higher-level reconciler determines whether or not the desired differs
-	// from the latest observed and decides whether to call the resource
-	// manager's Update method
-	Update(
-		context.Context,
-		AWSResource, /* desired */
-		AWSResource, /* latest */
-		*ackcompare.Delta,
-	) (AWSResource, error)
-
-	// Delete attempts to destroy the supplied AWSResource in the backend AWS
-	// service API, returning an AWSResource representing the
-	// resource being deleted (if delete is asynchronous and takes time)
+	Update(context.Context, AWSResource, AWSResource, *ackcompare.Delta) (AWSResource, error)
 	Delete(context.Context, AWSResource) (AWSResource, error)
-	// ARNFromName returns an AWS Resource Name from a given string name. This
-	// is useful for constructing ARNs for APIs that require ARNs in their
-	// GetAttributes operations but all we have (for new CRs at least) is a
-	// name for the resource
 	ARNFromName(string) string
-	// LateInitialize returns an AWS Resource after setting the late initialized
-	// fields from the ReadOne call. This method will initialize the optional fields
-	// which were not provided by the k8s user but were defaulted by the AWS service.
-	// If there are no such fields to be initialized, the returned object is identical to
-	// object passed in the parameter.
-	// This method also adds/updates the ConditionTypeLateInitialized for the AWSResource.
 	LateInitialize(context.Context, AWSResource) (AWSResource, error)
-	// ResolveReferences finds if there are any Reference field(s) present
-	// inside AWSResource passed in the parameter and attempts to resolve
-	// those reference field(s) into target field(s).
-	// It returns an AWSResource with resolved reference(s), and an error if the
-	// passed AWSResource's reference field(s) cannot be resolved.
-	// This method also adds/updates the ConditionTypeReferencesResolved for the
-	// AWSResource.
 	ResolveReferences(context.Context, client.Reader, AWSResource) (AWSResource, error)
-	// IsSynced returns true if a resource is synced.
 	IsSynced(context.Context, AWSResource) (bool, error)
-	// EnsureTags ensures that tags are present inside the AWSResource.
-	// If the AWSResource does not have any existing resource tags, the 'tags'
-	// field is initialized and the controller tags are added.
-	// If the AWSResource has existing resource tags, then controller tags are
-	// added to the existing resource tags without overriding them.
-	// If the AWSResource does not support tags, only then the controller tags
-	// will not be added to the AWSResource.
 	EnsureTags(context.Context, AWSResource, ServiceControllerMetadata) error
 }
 
